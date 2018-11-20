11cec209bb21f402073ed6f26a4f3e4148dda71e
Nick <nickolasupwork@gmail.com>
2018-11-20 14:29:42 +0300

    Это наш первый коммит

---
+++ lesson 1/script.js
@@ -0,0 +1,56 @@
+'use strict';
+// 
+let money = +prompt("Ваш бюджет на месяц?"),
+    time = +prompt("Введите дату в формате YYYY-MM-DD"),
+appData = {
+ budget: money,
+ timeData: time,
+ expenses: {},
+ optionalExpenses: {},
+ income: [],
+ savings: false
+};
+
+
+let a = prompt("Введите обязательную статью расходов в этом месяце"),
+    c = prompt("Введите обязательную статью расходов в этом месяце"),
+    b = +prompt("Во сколько обойдется?"),
+    d = +prompt("Во сколько обойдется?")
+
+
+let expenses = {
+ c : d
+};
+
+console.log(appData.budget / 30)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert("Hello fuck");
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+// let answer = +prompt("Есть ли Вам 18?", "Да")
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+// console.log(4 + " - object")
+
+
+
+
+
